e50ca599540a2c12a23347b4b39c829adbba96ce
revanthrk2004 <revanthkkrishnan@gmail.com>
2025-03-23 05:11:32 +0000

    Remove password field from User model

--- models/user_model.py
+++ models/user_model.py
@@ -7,7 +7,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(256), nullable=False)
+    # password = db.Column(...) ❌ Delete or comment this
+    password_hash = db.Column(db.String(512), nullable=False)
+
 
 
     def set_password(self, password):
