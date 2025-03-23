215864fdf5098f9ae7ab6affea5922bdbdd30c14
revanthrk2004 <revanthkkrishnan@gmail.com>
2025-03-23 04:48:51 +0000

    Fix table redefinition error for User model

--- models/user_model.py
+++ models/user_model.py
@@ -3,7 +3,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     __tablename__ = 'user'
-
+    __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
