65e91d982feb00797c321e46215867eb91f2cef6
revanthrk2004 <revanthkkrishnan@gmail.com>
2025-03-23 04:36:18 +0000

    Add set_password method to User model

--- models/user_model.py
+++ models/user_model.py
@@ -1,12 +1,14 @@
-from database.db_manager import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
-    __tablename__ = 'user'
-    __table_args__ = {'extend_existing': True}
-
+    __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), nullable=False)
+    username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
