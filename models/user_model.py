cb873c225e4602ebe3de5186e15748b646f36616
revanthrk2004 <revanthkkrishnan@gmail.com>
2025-03-23 05:30:40 +0000

    Drop password column from user table

--- models/user_model.py
+++ models/user_model.py
@@ -4,13 +4,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 class User(db.Model):
     __tablename__ = 'user'
     __table_args__ = {'extend_existing': True}
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    # password = db.Column(...) ❌ Delete or comment this
     password_hash = db.Column(db.String(512), nullable=False)
-
-
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
