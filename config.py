54730cc269b2a9e2ac492ab989b0b9eb0cc58e56
revanthrk2004 <revanthkkrishnan@gmail.com>
2025-05-13 22:47:36 +0100

    app.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os
-from datetime import timedelta  # Ensure this is imported
+from datetime import timedelta  
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
 
-    # ✅ Use External PostgreSQL URL (Not Internal)
+    
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
         "postgresql://revanthrk:NYylTjDRlIXvp14G6U4dlZnGEFpm2zaZ@dpg-cv16j6d6l47c73f3ultg-a.oregon-postgres.render.com/phonelert_db"
     )
 
-    # Ensure SQLAlchemy uses the correct format (Render sometimes gives `postgres://`)
+    
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
